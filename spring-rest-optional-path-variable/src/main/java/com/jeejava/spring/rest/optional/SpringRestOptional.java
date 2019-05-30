2c12634ff016dcf386bf4429376a8c84a0fdc0b0
Soumitra Roy Sarkar <projwebinfo@gmail.com>
2019-05-30 20:25:20 +0530

    Add files via upload

---
+++ spring-rest-optional-path-variable/src/main/java/com/jeejava/spring/rest/optional/SpringRestOptional.java
@@ -0,0 +1,13 @@
+package com.jeejava.spring.rest.optional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.jeejava.spring.rest.optional")
+public class SpringRestOptional {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestOptional.class, args);
+	}
+
+}
