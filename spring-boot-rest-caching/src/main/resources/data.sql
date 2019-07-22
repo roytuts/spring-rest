2dd97bd43284ce8a231eb18cdfda02b9774fd249
Soumitra Roy Sarkar <projwebinfo@gmail.com>
2019-07-22 12:35:15 +0530

    Add files via upload

---
+++ spring-boot-rest-caching/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into user(name,address)
+values('Soumitra','Earth');
+insert into user(name,address)
+values('Liton','Mars');
+insert into user(name,address)
+values('Suman','Jupiter');
+insert into user(name,address)
+values('Debabrata','Neptune');
