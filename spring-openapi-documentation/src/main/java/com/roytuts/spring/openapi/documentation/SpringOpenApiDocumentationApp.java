b9dd8eccaf86ecac46d0ec52e15406f4d1f31349
Soumitra Roy Sarkar <projwebinfo@gmail.com>
2020-02-27 09:27:13 +0530

    Add files via upload

---
+++ spring-openapi-documentation/src/main/java/com/roytuts/spring/openapi/documentation/SpringOpenApiDocumentationApp.java
@@ -0,0 +1,13 @@
+package com.roytuts.spring.openapi.documentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOpenApiDocumentationApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOpenApiDocumentationApp.class, args);
+	}
+
+}
