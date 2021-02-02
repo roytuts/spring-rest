672d8df929651acdafdc605d44ff5642323b4f04
Soumitra <email@email.com>
2021-02-02 08:13:15 +0530

    moved to https://github.com/roytuts/spring-cache/tree/master/spring-boot-rest-caching

--- spring-boot-rest-caching/src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-insert into user(name,address)
-values('Soumitra','Earth');
-insert into user(name,address)
-values('Liton','Mars');
-insert into user(name,address)
-values('Suman','Jupiter');
-insert into user(name,address)
-values('Debabrata','Neptune');
