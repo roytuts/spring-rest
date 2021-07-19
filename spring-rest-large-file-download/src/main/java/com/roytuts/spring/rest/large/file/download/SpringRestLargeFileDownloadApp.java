c4bda37940ad9b92687e212b6f5caa92824a5efb
Soumitra <email@email.com>
2021-07-19 10:21:25 +0530

    spring rest large file download

---
+++ spring-rest-large-file-download/src/main/java/com/roytuts/spring/rest/large/file/download/SpringRestLargeFileDownloadApp.java
@@ -0,0 +1,13 @@
+package com.roytuts.spring.rest.large.file.download;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestLargeFileDownloadApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestLargeFileDownloadApp.class, args);
+	}
+
+}
---
+++ spring-rest-large-file-download/src/main/java/com/roytuts/spring/rest/large/file/download/dto/Input.java
@@ -0,0 +1,24 @@
+package com.roytuts.spring.rest.large.file.download.dto;
+
+public class Input {
+
+	private String downloadUrl;
+	private String downloadPath;
+
+	public String getDownloadUrl() {
+		return downloadUrl;
+	}
+
+	public void setDownloadUrl(String downloadUrl) {
+		this.downloadUrl = downloadUrl;
+	}
+
+	public String getDownloadPath() {
+		return downloadPath;
+	}
+
+	public void setDownloadPath(String downloadPath) {
+		this.downloadPath = downloadPath;
+	}
+
+}
