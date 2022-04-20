01574b76c52afbabe59bb266313612d6e42e914e
Soumitra <projwebinfo@gmail.com>
2022-04-20 17:23:49 +0530

    Create AppConstants.java

---
+++ spring-web-exception-handling/src/main/java/com/roytuts/spring/web/exception/handling/constants/AppConstants.java
@@ -0,0 +1,11 @@
+package com.roytuts.spring.web.exception.handling.constants;
+
+public final class AppConstants {
+
+	private AppConstants() {
+	}
+
+	public final static String SUCCESS = "SUCCESS";
+	public final static String INCORRECT_RESULT_SIZE = "Incorrect result size";
+
+}
