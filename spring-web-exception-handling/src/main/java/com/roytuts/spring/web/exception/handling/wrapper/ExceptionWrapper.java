d50e831d2265ece465e1edd468c42c7045a2a1cc
Soumitra <projwebinfo@gmail.com>
2022-04-20 17:28:19 +0530

    Create ExceptionWrapper.java

---
+++ spring-web-exception-handling/src/main/java/com/roytuts/spring/web/exception/handling/wrapper/ExceptionWrapper.java
@@ -0,0 +1,29 @@
+package com.roytuts.spring.web.exception.handling.wrapper;
+
+public class ExceptionWrapper {
+
+	private String code;
+	private String message;
+
+	public ExceptionWrapper(String code, String message) {
+		this.code = code;
+		this.message = message;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
