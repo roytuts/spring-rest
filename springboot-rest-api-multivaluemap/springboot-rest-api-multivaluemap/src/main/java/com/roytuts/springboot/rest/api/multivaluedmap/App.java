a2c650a3e88bdb1d7c8c8397e214ff74f95355ba
Soumitra <projwebinfo@gmail.com>
2022-06-23 11:03:24 +0530

    Create App.java

---
+++ springboot-rest-api-multivaluemap/springboot-rest-api-multivaluemap/src/main/java/com/roytuts/springboot/rest/api/multivaluedmap/App.java
@@ -0,0 +1,13 @@
+package com.roytuts.springboot.rest.api.multivaluedmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
