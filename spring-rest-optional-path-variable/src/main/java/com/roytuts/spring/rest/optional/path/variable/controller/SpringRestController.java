cfd6b769c1d14faf1ab7124c11b5c545d4490b1e
Soumitra <projwebinfo@gmail.com>
2023-07-03 18:49:46 +0530

    Create SpringRestController.java

---
+++ spring-rest-optional-path-variable/src/main/java/com/roytuts/spring/rest/optional/path/variable/controller/SpringRestController.java
@@ -0,0 +1,26 @@
+package com.roytuts.spring.rest.optional.path.variable.controller;
+
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringRestController {
+	
+	@GetMapping(value = { "/hi/{name}", "/hi" })
+	public ResponseEntity<String> getHi(@PathVariable(required = false) String name) {
+		return new ResponseEntity<String>(
+				"Hi " + (name == null || name.length() == 0 ? "Anonymous" : name) + ", Good Morning!", HttpStatus.OK);
+	}
+
+	@GetMapping(value = { "/hello/{name}", "/hello" })
+	public ResponseEntity<String> getHello(@PathVariable Optional<String> name) {
+		return new ResponseEntity<String>("Hello " + (name.isPresent() ? name.get() : "Anonymous") + ", Good Morning!",
+				HttpStatus.OK);
+	}
+
+}
