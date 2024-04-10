12ef6c356480c1fff8a5e76e23737d81a460efb8
Soumitra <projwebinfo@gmail.com>
2024-04-10 16:06:05 +0530

    Create Employee.java

---
+++ spring-rest-retry-recover/spring-rest-retry/src/main/java/com/roytuts/springrestretry/model/Employee.java
@@ -0,0 +1,49 @@
+package com.roytuts.springrestretry.model;
+
+public class Employee {
+
+	private Integer id;
+
+	private String employeeName;
+
+	private Double employeeSalary;
+
+	public Employee() {
+	}
+
+	public Employee(Integer id, String employeeName, Double employeeSalary) {
+		this.id = id;
+		this.employeeName = employeeName;
+		this.employeeSalary = employeeSalary;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public Double getEmployeeSalary() {
+		return employeeSalary;
+	}
+
+	public void setEmployeeSalary(Double employeeSalary) {
+		this.employeeSalary = employeeSalary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", employeeName=" + employeeName + ", employeeSalary=" + employeeSalary + "]";
+	}
+
+}
