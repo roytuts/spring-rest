655d3a22a11f9cfca8ac5ca3b2f71eb3fb05e15f
Soumitra <projwebinfo@gmail.com>
2024-04-10 16:08:53 +0530

    Create ServerApp.java

---
+++ spring-rest-retry-recover/spring-rest-server/src/main/java/com/roytuts/springrestserver/ServerApp.java
@@ -0,0 +1,13 @@
+package com.roytuts.springrestserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApp.class, args);
+	}
+
+}
